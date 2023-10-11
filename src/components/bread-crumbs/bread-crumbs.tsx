3b3844998693fef466bcd80535f246b91196fbf0
Orixena <encephalon666@yandex.ru>
2023-10-11 21:19:00 +0300

    breadCrumbs

--- src/components/bread-crumbs/bread-crumbs.tsx
+++ src/components/bread-crumbs/bread-crumbs.tsx
@@ -1,21 +1,46 @@
+import { useAppSelector } from '../hooks';
+import { getActiveProduct } from '../../store/active-product-data/active-product-data.selectors';
+import { AppRoute } from '../../const';
+
 function BreadCrumbs(): JSX.Element {
+  const product = useAppSelector(getActiveProduct);
+
   return (
     <div className="breadcrumbs">
       <div className="container">
         <ul className="breadcrumbs__list">
           <li className="breadcrumbs__item">
-            <a className="breadcrumbs__link" href="index.html">
+            <a className="breadcrumbs__link" href={AppRoute.Main}>
               Главная
               <svg width={5} height={8} aria-hidden="true">
                 <use xlinkHref="#icon-arrow-mini" />
               </svg>
             </a>
           </li>
-          <li className="breadcrumbs__item">
-            <span className="breadcrumbs__link breadcrumbs__link--active">
-              Каталог
-            </span>
-          </li>
+          {product ? (
+            <>
+              <li className="breadcrumbs__item">
+                <a className="breadcrumbs__link" href={AppRoute.Main}>
+                  Каталог
+                  <svg width={5} height={8} aria-hidden="true">
+                    <use xlinkHref="#icon-arrow-mini" />
+                  </svg>
+                </a>
+              </li>
+
+              <li className="breadcrumbs__item">
+                <span className="breadcrumbs__link breadcrumbs__link--active">
+                  {product.name}
+                </span>
+              </li>
+            </>
+          ) : (
+            <li className="breadcrumbs__item">
+              <span className="breadcrumbs__link breadcrumbs__link--active">
+                Каталог
+              </span>
+            </li>
+          )}
         </ul>
       </div>
     </div>
